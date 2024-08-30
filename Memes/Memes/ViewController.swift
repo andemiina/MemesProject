ce2fc5eafdfd9cc8efca6fbcb881a172563d884b
Anna <andemiina2020@gmail.com>
2024-08-30 16:58:47 +0300

    сверстала основной экран

--- Memes/Memes/ViewController.swift
+++ Memes/Memes/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var jokeIDLabel: UILabel!
+    @IBOutlet weak var jokeIDNumber: UILabel!
+    private var isTapped: Bool = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        setUpFonts()
+        
+        jokeIDLabel.font = UIFont(name: "Roboto-Bold", size: 16)
+        jokeIDNumber.font = UIFont(name: "Roboto-Bold", size: 16)
+        typeLabel.font = UIFont(name: "Roboto-Bold", size: 16)
+ 
+
     }
 
-
+    @IBAction func refreshButtonClicked(_ sender: Any) {
+        if isTapped {
+            jokeIDLabel.text = "tapped"
+        } else {
+            jokeIDLabel.text = "tap"
+        }
+        isTapped.toggle()
+    }
+    
+    
+//    private func setUpFonts() {
+//        jokeIDLabel.font = UIFont(name: "Roboto-Bold", size: 16)
+//    }
+//    
 }
 
